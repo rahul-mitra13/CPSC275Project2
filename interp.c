c25d3db15ffc22ffd54d50312b391cd92f9799e2
Rahul Mitra (rmitra) <rmitra@syslab22.cs.trincoll.edu>
2019-10-28 21:14:42 -0400

    added more stuff

--- interp.c
+++ interp.c
@@ -7,10 +7,23 @@ void main(){
   int registers[NumRegisters];//registers
   int memmory[NumMemoryLocations];//memory NumMemoryLocations
   char instruction[MaxInstructionLength];//inputted instruction
-  char toQuit[] = {'Q','U','I','T','\0'};
+  char *toRead = "READ";
+  char *toWrite = "WRITE";
+  char *toPrint = "PRINTS";
+  char *toMove = "MOVE";
+  char *toAdd = "ADD";
+  char *toSubtract = "SUB";
+  char *toMultiply = "MUL"
+  char *toDivide = "DIV";
+  char *toMod = "MOD";
+  char *toCompare = "COMP";
+  char *toQuit = "QUIT";
   int zf = 0;//zero flag
   int sg = 0;//sign flag
   int of = 0;//overflow flag
   scanf("%s",instruction);
-  printf("This is what you input = %d\n", strcmp(instruction,toQuit));
+  while (strcmp(instruction, toQuit) != 0){
+    printf("This is what you entered = %s\n", instruction);
+    scanf("%s",instruction);
+  }
 }
