d0f28ede8e3cdbb2b467bbe96a0579b9ae6280c7
Rahul Mitra (rmitra) <rmitra@syslab22.cs.trincoll.edu>
2019-10-28 20:57:59 -0400

    made preliminary progress

--- interp.c
+++ interp.c
@@ -1,1 +1,16 @@
 #include<stdio.h>
+#include<string.h>
+#define NumRegisters 4
+#define NumMemoryLocations 8
+#define MaxInstructionLength 8
+void main(){
+  int registers[NumRegisters];//registers
+  int memmory[NumMemoryLocations];//memory NumMemoryLocations
+  char instruction[MaxInstructionLength];//inputted instruction
+  char toQuit[] = {'Q','U','I','T','\0'};
+  int zf = 0;//zero flag
+  int sg = 0;//sign flag
+  int of = 0;//overflow flag
+  scanf("%s",instruction);
+  printf("This is what you input = %d\n", strcmp(instruction,toQuit));
+}
