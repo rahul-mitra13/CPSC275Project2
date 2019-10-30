f01b56708e61225effd5e366db9ca101b6efae8f
Rahul Mitra <rmitra@trincoll.edu>
2019-10-30 00:40:40 -0400

    everything is broken

--- interp.c
+++ interp.c
@@ -5,12 +5,14 @@
 #define MaxInstructionLength 8
 #define numFlags 3
 void print(int a[],int b[], int c[]);
-void main(){
+// void read(int instruction[], int memory[], int registers[])
+int main(){
   //asci value of question mark is 63
   int registers[NumRegisters] = {63,63,63,63};//registers
   int memory[NumMemoryLocations] = {63,63,63,63,63,63,63,63};//memory NumMemoryLocations
   int flags[numFlags] = {0, 0, 0};//to store all the flags
   char instruction[MaxInstructionLength];//inputted raw instruction
+  char *ptr;//this will act as a test pointer when looking for instructions
   char *toRead = "READ";
   char *toWrite = "WRITE";
   char *toPrint = "PRINTS";
@@ -30,9 +32,14 @@ void main(){
     if (strcasecmp(instruction, toPrint) == 0){//if the input command is to print
       print(registers, memory, flags);
     }
+    //the above two are the only single worded instructions
+    if ( strcasecmp(instruction, toRead) == 0){
+      //read(instruction,memory,registers);
+      printf("You entered a read instruction\n");
+    }
     scanf("%s",instruction);
-
   }
+  return 0;
 }
 /*function that is called when input is prints*/
 void print(int a[], int b[], int c[]){
