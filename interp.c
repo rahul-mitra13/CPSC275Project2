8771bf0b8b397dd67ba6d6edff4bd0f0dc8fbdb0
Rahul Mitra <rmitra@trincoll.edu>
2019-10-31 01:16:16 -0400

    can read strings now

--- interp.c
+++ interp.c
@@ -5,50 +5,64 @@
 #define MaxInstructionLength 8
 #define numFlags 3
 void print(int a[],int b[], int c[]);
-// void read(int instruction[], int memory[], int registers[])
+int split(char a[],char b[3][8]);//where a is the original string and b is the tokenized string
 int main(){
-  //asci value of question mark is 63
-  int registers[NumRegisters] = {63,63,63,63};//registers
-  int memory[NumMemoryLocations] = {63,63,63,63,63,63,63,63};//memory NumMemoryLocations
+  int registers[NumRegisters] = {128,128,128,128};//registers
+  int memory[NumMemoryLocations] = {128,128,128,128,128,128,128,128};//memory NumMemoryLocations
   int flags[numFlags] = {0, 0, 0};//to store all the flags
   char instruction[MaxInstructionLength];//inputted raw instruction
-  char *ptr;//this will act as a test pointer when looking for instructions
-  char *toRead = "READ";
-  char *toWrite = "WRITE";
-  char *toPrint = "PRINTS";
-  char *toMove = "MOVE";
-  char *toAdd = "ADD";
-  char *toSubtract = "SUB";
-  char *toMultiply = "MUL";
-  char *toDivide = "DIV";
-  char *toMod = "MOD";
-  char *toCompare = "COMP";
-  char *toQuit = "QUIT";
+  char instructionAfterParsing[3][8];//instruction stored after parsing
+  int size;
+  int i;
+  char toRead[] = "READ";
+  char toWrite[] = "WRITE";
+  char toPrint[] = "PRINTS";
+  char toMove[] = "MOVE";
+  char toAdd[] = "ADD";
+  char toSubtract[] = "SUB";
+  char toMultiply[] = "MUL";
+  char toDivide[] = "DIV";
+  char toMod[] = "MOD";
+  char toCompare[] = "COMP";
+  char toQuit[] = "QUIT";
   int zf = 0;//zero flag
   int sg = 0;//sign flag
   int of = 0;//overflow flag
-  scanf("%s",instruction);
-  while (strcasecmp(instruction, toQuit) != 0){//keep going until the user enters quit
-    if (strcasecmp(instruction, toPrint) == 0){//if the input command is to print
-      print(registers, memory, flags);
+  int tester;
+  scanf(" %[^\n]s",instruction);
+  size = split(instruction, instructionAfterParsing);
+  printf("This is the size %d\n",size);
+      for (i = 0; i < size; i++){
+        printf("%s\n",instructionAfterParsing[i]);
+      }
+  scanf("%d",&tester);
+  scanf(" %[^\n]s",instruction);
+  size = split(instruction, instructionAfterParsing);
+  printf("This is the size %d\n",size);
+    for (i = 0; i < size; i++){
+        printf("%s\n",instructionAfterParsing[i]);
     }
-    //the above two are the only single worded instructions
-    if ( strcasecmp(instruction, toRead) == 0){
-      //read(instruction,memory,registers);
-      printf("You entered a read instruction\n");
-    }
-    scanf("%s",instruction);
-  }
-  return 0;
+    return 0;
 }
+
 /*function that is called when input is prints*/
 void print(int a[], int b[], int c[]){
   int i;
   for ( i = 0; i < NumRegisters; i++){
-       printf("%c\t",a[i]);
+       if (a[i] > 127 || a[i] < -128){
+       printf("?\t");
+       }
+       else{
+       printf("%d\t",a[i]);
+       }
   }
   for ( i = 0; i < NumMemoryLocations; i++){
+    if ( b[i] > 127 || b[i] < -128){
+      printf("?\t");
+    }
+    else{
     printf("%c\t",b[i]);
+    }
   }
   for ( i = 0; i < numFlags; i++){
     printf("%d\t",c[i]);
@@ -65,4 +79,14 @@ void print(int a[], int b[], int c[]){
   printf("SF\t");
   printf("OF\t");
   printf("\n");
+}
+/*this function tokenizes a string */
+int split(char a[], char b[3][8]){
+  int k = 0;
+  char* token = strtok(a," ,\n");
+  while (token){
+    strcpy(b[k++],token);
+    token = strtok(NULL," ,\n");
+  }
+  return k;
 }
