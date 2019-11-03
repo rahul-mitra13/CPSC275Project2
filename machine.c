f7fd012bed9d3cd14b99842a47be659df75f4def
Rahul Mitra <rmitra@trincoll.edu>
2019-11-03 18:38:21 -0500

    got the whole program in a 2D array

--- machine.c
+++ machine.c
@@ -7,7 +7,7 @@
 #define MaxInstructionLength 8
 #define numFlags 3
 void print(int a[],int b[], int c[]);
-int split(char a[],char b[100][100], char c[100][100]);//where a is the original string and b is the tokenized string
+int split(char a[],char c[100][100]);//where a is the original string and c is the tokenized string without coomments
 void read(int a[], int pos, int num);
 void write(int a[], int pos);
 void add(int flags[],int a[], int b[], int num1, int c[], int num2);
@@ -22,11 +22,12 @@ int main(){
   int flags[numFlags] = {0, 0, 0};//to store all the flags
   int registers[NumRegisters] = {128,128,128,128};//registers
   int memory[NumMemoryLocations] = {128,128,128,128,128,128,128,128};//memory NumMemoryLocations
-  char instruction[100];//inputted raw instruction
-  char inter[100][100];//intermediate array to store tokens with comments
+  char instruction[100];
   char instructionAfterParsing[100][100];//instruction stored after parsing and removing comments
+  char wholeProgram[100][100];
   int size;
   int i;
+  int k;
   int numlines;
   char toRead[] = "READ";
   char toWrite[] = "WRITE";
@@ -40,271 +41,26 @@ int main(){
   char toCompare[] = "COMP";
   char toQuit[] = "QUIT";
   char toStart[] = ".start:";
+  char newstr[100];
+  k = 0;
   printf("\n");
-  numlines = 0;
-  while (1){//infinite loop until user quits
-    scanf(" %[^\n]s",instruction);
-    size = split(instruction,inter,instructionAfterParsing);
-    numlines++;
-    //handle the case for the first line 
-    //on the first line, you can only print, quit and write - nothing else makes sense
-    if (numlines == 1){
-        if ( strcasecmp(instructionAfterParsing[0], toStart) != 0){
-            printf("hello1???\n");
-        }
-    //quit  command
-    else if ((strcasecmp(instructionAfterParsing[1],toQuit) == 0) && size == 2){//if the command is to quit
-      break;
+  while ((scanf(" %[^\n]s",instruction)) != EOF){
+  size = split(instruction, instructionAfterParsing);
+  newstr[0] = '\0';
+  for ( i = 0; i < size; i++){
+          if ( i == size - 1){
+          strcat(newstr,strcat(instructionAfterParsing[i]," "));
+          }
+          else{
+              strcat(newstr,strcat(instructionAfterParsing[i]," "));
+          }
+      }
+      strcpy(wholeProgram[k++],newstr);
     }
-    else if ((strcasecmp(instructionAfterParsing[1],toQuit) == 0) && size != 2){//syntax error if there is any argument to quit
-      if ( size != 1){
-      printf("hello2??\n");
-      printf("\n");
-      }
-    }
-    //print command
-    else if ((strcasecmp(instructionAfterParsing[1],toPrint) == 0) && size == 2){//if the command is to print
-      print(registers,memory,flags);
-    }
-    else if ((strcasecmp(instructionAfterParsing[1],toPrint) == 0) && size != 2){//syntax error if there is any argument to print
-      if ( size != 1){
-      printf("hello3???\n");
-      printf("\n");
-      }
-    }
-    //read command
-    else if ((strcasecmp(instructionAfterParsing[1],toRead) == 0) && size == 4){//if the command is to print
-      if ( tolower(instructionAfterParsing[3][0]) == 'm'){
-        read(memory,(instructionAfterParsing[3][1]-'0'),atoi(instructionAfterParsing[2]));//goes array,position,num
-      }
-      else if ( tolower(instructionAfterParsing[3][0]) == 'r'){
-        read(registers,(instructionAfterParsing[3][1]-'0'),atoi(instructionAfterParsing[2]));//goes array,position,num
-      }
-    }
-    else if ((strcasecmp(instructionAfterParsing[0],toRead) == 0) && size != 4){//if there is an illegal number of arguments
-       if ( size != 1){
-        printf("hello4???\n");
-        printf("\n");
-       }
-    }
-    //write command
-    else if ((strcasecmp(instructionAfterParsing[1],toWrite)) == 0 && size == 3){//if the input command is to write
-      if (tolower(instructionAfterParsing[2][0]) == 'm'){
-        write(memory,(instructionAfterParsing[2][1] - '0'));
-      }
-      else if (tolower(instructionAfterParsing[2][0]) == 'r'){
-        write(registers, (instructionAfterParsing[2][1] - '0'));
-      }
-    }
-    else if ((strcasecmp(instructionAfterParsing[1],toWrite)) == 0 && size != 3){//illegal number of arguments
-     if ( size != 1){
-      printf("hello5???\n");
-      printf("\n");
-     }
-    }
-    //else invalid command
-    else if (size != 1){
-      printf("hello6???\n");
-      printf("\n");
-      }
-    }
-    //end of first line checking
-    else {
-    //quit command
-    if ((strcasecmp(instructionAfterParsing[0],toQuit) == 0) && size == 1){//if the command is to quit
-      break;
-    }
-    else if ((strcasecmp(instructionAfterParsing[0],toQuit) == 0) && size != 1){//syntax error if there is any argument to quit
-      if ( size != 0){
-      printf("hello7???\n");
-      printf("\n");
-      }
-    }
-    //print command
-    else if ((strcasecmp(instructionAfterParsing[0],toPrint) == 0) && size == 1){//if the command is to print
-      print(registers,memory,flags);
-    }
-    else if ((strcasecmp(instructionAfterParsing[0],toPrint) == 0) && size != 1){//syntax error if there is any argument to print
-      if ( size != 0){
-      printf("hello8???\n");
-      printf("\n");
-      } 
-    }
-    //read command
-    else if ((strcasecmp(instructionAfterParsing[0],toRead) == 0) && size == 3){//if the command is to print
-      if ( tolower(instructionAfterParsing[2][0]) == 'm'){
-        read(memory,(instructionAfterParsing[2][1]-'0'),atoi(instructionAfterParsing[1]));//goes array,position,num
-      }
-      else if ( tolower(instructionAfterParsing[2][0]) == 'r'){
-        read(registers,(instructionAfterParsing[2][1]-'0'),atoi(instructionAfterParsing[1]));//goes array,position,num
-      }
-    }
-    else if ((strcasecmp(instructionAfterParsing[0],toRead) == 0) && size != 3){//if there is an illegal number of arguments
-        if ( size != 0){
-        printf("hello9???\n");
-        printf("\n");
-        }
-    }
-    //write command
-    else if ((strcasecmp(instructionAfterParsing[0],toWrite)) == 0 && size == 2){//if the input command is to write
-      if (tolower(instructionAfterParsing[1][0]) == 'm'){
-        write(memory,(instructionAfterParsing[1][1] - '0'));
-      }
-      else if (tolower(instructionAfterParsing[1][0]) == 'r'){
-        write(registers, (instructionAfterParsing[1][1] - '0'));
-      }
-    }
-    else if ((strcasecmp(instructionAfterParsing[0],toWrite)) == 0 && size != 2){//illegal number of arguments
-      if ( size != 0){
-      printf("hello10???\n");
-      printf("\n");
-      }
-    }
-    //add command
-    else if ((strcasecmp(instructionAfterParsing[0], toAdd) == 0) && size == 3){
-      if ( tolower(instructionAfterParsing[1][0]) == 'm' && tolower(instructionAfterParsing[2][0]) == 'r'){
-        add(flags,registers, memory, (instructionAfterParsing[1][1] - '0'), registers, (instructionAfterParsing[2][1] - '0'));
-      }
-      else if  ( tolower(instructionAfterParsing[1][0]) == 'r' && tolower(instructionAfterParsing[2][0]) == 'm'){
-        add(flags,registers,registers,(instructionAfterParsing[1][1] - '0'), memory, (instructionAfterParsing[2][1] - '0'));
-      }
-      else if  ( tolower(instructionAfterParsing[1][0]) == 'r' && tolower(instructionAfterParsing[2][0]) == 'r'){
-        add(flags,registers,registers,(instructionAfterParsing[1][1] - '0'), registers, (instructionAfterParsing[2][1] - '0'));
-      }
-      else if ( tolower(instructionAfterParsing[1][0]) == 'm' && tolower(instructionAfterParsing[2][0]) == 'm'){
-        printf("hello11???\n");
-      }
-    }
-    else if ((strcasecmp(instructionAfterParsing[0], toAdd) == 0) && size != 3){//if the number of arguments are off
-       if ( size != 0){
-       printf("hello12???\n");
-       printf("\n");
-       }
-    }
-    //subtract command
-    else if ((strcasecmp(instructionAfterParsing[0], toSubtract) == 0) && size == 3){
-      if ( tolower(instructionAfterParsing[1][0]) == 'm' && tolower(instructionAfterParsing[2][0]) == 'r'){
-        subtract(flags,registers, memory, (instructionAfterParsing[1][1] - '0'), registers, (instructionAfterParsing[2][1] - '0'));
-      }
-      else if  ( tolower(instructionAfterParsing[1][0]) == 'r' && tolower(instructionAfterParsing[2][0]) == 'm'){
-        subtract(flags,registers,registers,(instructionAfterParsing[1][1] - '0'), memory, (instructionAfterParsing[2][1] - '0'));
-      }
-      else if  ( tolower(instructionAfterParsing[1][0])== 'r' && tolower(instructionAfterParsing[2][0]) == 'r'){
-        subtract(flags,registers,registers,(instructionAfterParsing[1][1] - '0'), registers, (instructionAfterParsing[2][1] - '0'));
-      }
-      else if ( tolower(instructionAfterParsing[1][0]) == 'm' && tolower(instructionAfterParsing[2][0]) == 'm'){
-        printf("hello13???\n");
-        printf("\n");
-      }
-    }
-    else if ((strcasecmp(instructionAfterParsing[0], toSubtract) == 0) && size != 3){//if the number of arguments are off
-       if ( size != 0){
-       printf("hello14???\n");
-       printf("\n");
-       }
-    }
-    //multiply command
-    else if ((strcasecmp(instructionAfterParsing[0], toMultiply) == 0) && size == 3){
-      if ( tolower(instructionAfterParsing[1][0]) == 'm' && tolower(instructionAfterParsing[2][0]) == 'r'){
-        multiply(flags,registers, memory, (instructionAfterParsing[1][1] - '0'), registers, (instructionAfterParsing[2][1] - '0'));
-      }
-      else if  ( tolower(instructionAfterParsing[1][0]) == 'r' && tolower(instructionAfterParsing[2][0]) == 'm'){
-        multiply(flags,registers,registers,(instructionAfterParsing[1][1] - '0'), memory, (instructionAfterParsing[2][1] - '0'));
-      }
-      else if  ( tolower(instructionAfterParsing[1][0]) == 'r' && tolower(instructionAfterParsing[2][0]) == 'r'){
-        multiply(flags,registers,registers,(instructionAfterParsing[1][1] - '0'), registers, (instructionAfterParsing[2][1] - '0'));
-      }
-      else if ( tolower(instructionAfterParsing[1][0]) == 'm' && tolower(instructionAfterParsing[2][0]) == 'm'){
-        printf("hello15???\n");
-        printf("\n");
-      }
-    }
-    else if ((strcasecmp(instructionAfterParsing[0], toMultiply) == 0) && size != 3){//if the number of arguments are off
-      if ( size != 0){
-       printf("hello16???\n");
-       printf("\n");
-      }
-    }
-    //divide command
-    else if ((strcasecmp(instructionAfterParsing[0], toDivide) == 0) && size == 3){
-      if ( tolower(instructionAfterParsing[1][0]) == 'm' && tolower(instructionAfterParsing[2][0]) == 'r'){
-        divide(flags,registers, memory, (instructionAfterParsing[1][1] - '0'), registers, (instructionAfterParsing[2][1] - '0'));
-      }
-      else if  ( tolower(instructionAfterParsing[1][0]) == 'r' && tolower(instructionAfterParsing[2][0]) == 'm'){
-        divide(flags,registers,registers,(instructionAfterParsing[1][1] - '0'), memory, (instructionAfterParsing[2][1] - '0'));
-      }
-      else if  ( tolower(instructionAfterParsing[1][0]) == 'r' && tolower(instructionAfterParsing[2][0]) == 'r'){
-        divide(flags,registers,registers,(instructionAfterParsing[1][1] - '0'), registers, (instructionAfterParsing[2][1] - '0'));
-      }
-      else if ( tolower(instructionAfterParsing[1][0]) == 'm' && tolower(instructionAfterParsing[2][0]) == 'm'){
-        printf("hello17???\n");
-        printf("\n");
-      }
-    }
-    else if ((strcasecmp(instructionAfterParsing[0], toDivide) == 0) && size != 3){//if the number of arguments are off
-       if ( size != 0){
-       printf("hello18???\n");
-       printf("\n");
-       }
-    }
-    //mod command
-    else if ((strcasecmp(instructionAfterParsing[0], toMod) == 0) && size == 3){
-      if ( tolower(instructionAfterParsing[1][0]) == 'm' && tolower(instructionAfterParsing[2][0]) == 'r'){
-        mod(flags,registers, memory, (instructionAfterParsing[1][1] - '0'), registers, (instructionAfterParsing[2][1] - '0'));
-      }
-      else if  ( tolower(instructionAfterParsing[1][0]) == 'r' && tolower(instructionAfterParsing[2][0]) == 'm'){
-        mod(flags,registers,registers,(instructionAfterParsing[1][1] - '0'), memory, (instructionAfterParsing[2][1] - '0'));
-      }
-      else if  ( tolower(instructionAfterParsing[1][0]) == 'r' && tolower(instructionAfterParsing[2][0]) == 'r'){
-        mod(flags,registers,registers,(instructionAfterParsing[1][1] - '0'), registers, (instructionAfterParsing[2][1] - '0'));
-      }
-      else if ( tolower(instructionAfterParsing[1][0]) == 'm' && tolower(instructionAfterParsing[2][0]) == 'm'){
-        printf("hello19???\n");
-        printf("\n");
-      }
-    }
-    else if ((strcasecmp(instructionAfterParsing[0], toMod) == 0) && size != 3){//if the number of arguments are off
-       if ( size != 0){
-       printf("hello20???\n");
-       printf("\n");
-       }
-    }
-    //move command
-    else if ((strcasecmp(instructionAfterParsing[0], toMove) == 0) && size == 3){
-      if ( tolower(instructionAfterParsing[1][0]) == 'm' && tolower(instructionAfterParsing[2][0]) == 'r'){
-        move(memory, (instructionAfterParsing[1][1] - '0'), registers, (instructionAfterParsing[2][1] - '0'));
-      }
-      else if  ( tolower(instructionAfterParsing[1][0]) == 'r' && tolower(instructionAfterParsing[2][0]) == 'm'){
-        move(registers,(instructionAfterParsing[1][1] - '0'), memory, (instructionAfterParsing[2][1] - '0'));
-      }
-      else if  ( tolower(instructionAfterParsing[1][0]) == 'r' && tolower(instructionAfterParsing[2][0]) == 'r'){
-        move(registers,(instructionAfterParsing[1][1] - '0'), registers, (instructionAfterParsing[2][1] - '0'));
-      }
-      else if (tolower(instructionAfterParsing[1][0]) == 'm' && tolower(instructionAfterParsing[2][0]) == 'm'){
-        printf("hello21???\n");
-        printf("\n");
-      }
-    }
-    else if ((strcasecmp(instructionAfterParsing[0], toMove) == 0) && size != 3){//if the number of arguments are off
-       if ( size != 0){ 
-       printf("hello22???\n");
-       printf("\n");
-       }
-    }
-    //comp command
-    else if ((strcasecmp(instructionAfterParsing[0], toCompare) == 0) && size == 3){
-        comp(flags, registers, (instructionAfterParsing[1][1] - '0'),(instructionAfterParsing[2][1] - '0'));
-    }
-    //else invalid command
-    else {
-            if ( size != 0){  
-            printf("hello23???\n");
-            printf("\n");
-            }
-        }
-    }//if not on the first line
-  }//end of while loop
-    return 0;
+  for ( i = 0; i < k; i++){
+      printf("%s\n",wholeProgram[i]);
+  }
+  printf("This is the size = %d\n",k);
 }
 
 /*function that is called when input is prints*/
@@ -344,9 +100,10 @@ void print(int a[], int b[], int c[]){
   printf("\n");
 }
 /*this function tokenizes a string */
-int split(char a[], char b[100][100], char c[100][100]){
+int split(char a[], char c[100][100]){
   int k = 0;//number of tokens including comments
   int ctr = 0;//number of comments excluding comments
+  char b[100][100];
   int i;
   char* token = strtok(a," ,\n");
   while (token){
